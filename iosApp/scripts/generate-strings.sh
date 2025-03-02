f5a3e425dd96cbd1c8f06e15c40b25183fddd3ec
BigamitionGit <hosoda-hiroshi@dmm.com>
2025-03-02 21:05:05 +0900

    パスを修正

--- iosApp/scripts/generate-strings.sh
+++ iosApp/scripts/generate-strings.sh
@@ -11,6 +11,6 @@ bundle install
 # convert strings.xml to Localizable.xcstrings
 echo "↔️  Converting strings.xml to Localizable.xcstrings..."
 iosApp/scripts/generate-strings.rb \
-  ../features/reminder/src/androidMain/res/values-ja/strings.xml \
-  ../features/reminder/src/androidMain/res/values/strings.xml \
-  Feature/Sources/ReminderFeature/Resources/Localizable.xcstrings
+  features/reminder/src/androidMain/res/values-ja/strings.xml \
+  features/reminder/src/androidMain/res/values/strings.xml \
+  iosApp/Feature/Sources/ReminderFeature/Resources/Localizable.xcstrings
